abaaab7a3d23e92f7f7942aaa16728e8255faf6c
sujeet <jeetsahu65@gmail.com>
2023-01-29 18:39:54 +0530

    lastone

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Navbar from './components/Navbar';
 import React, { Component } from 'react'
 import News from './components/News';
 
-export default class App extends Component {
+export default class App extends Component { 
   render() {
     return (
       <>
